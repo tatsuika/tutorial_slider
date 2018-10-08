f244a152e3bc0dcb9fbbfbdfaa55e31383973376
Tatsuika <tatu3939104@gmail.com>
2018-10-08 13:35:39 +0900

    Initial Commit

---
+++ tutorial_slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tutorial_slider
+//
+//  Created by 猪飼　立晟 on 2018/10/08.
+//  Copyright © 2018年 Tatsuika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
